01a45a34fed3f344de6d8da9512c1f4935da91c0
satya sindhu <movva001@gmail.com>
2022-01-06 19:50:08 -0500

    add students query using pg

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -2,28 +2,21 @@ const { Pool } = require('pg');
 
 
 const pool = new Pool({
-  user: 'satya sindhu movva',
-  password: '123',
+  user: 'labber',
+  password: 'labber',
   host: 'localhost',
   database: 'bootcampx'
 });
 
-
-const queryString = `
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE $1
-  LIMIT $2;
-  `;
-
-const cohortName = process.argv[2];
-const limit = process.argv[3] || 5;
-const values = [`%${cohortName}%`, limit];
-
-pool.query(queryString, values)
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  });
+  })
 }).catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -2,29 +2,25 @@ const { Pool } = require('pg');
 
 
 const pool = new Pool({
-  user: 'satya sindhu movva',
-  password: '123',
+  user: 'labber',
+  password: 'labber',
   host: 'localhost',
   database: 'bootcampx'
 });
 
-const query = `
+pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE $1
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
 ORDER BY teacher;
-`
-
-const cohort = process.argv[2] || 'JUL02';
-const values = [cohort];
-
-pool.query(query, values)
+`)
 .then(res => {
+  console.log(res);
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
-  });
-});
+  })
+})
 .catch(err => console.error('query error', err.stack));
