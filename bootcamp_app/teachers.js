2028de1639b3e1a991d9bdbe87ec24172a87342f
satya sindhu <movva001@gmail.com>
2022-01-06 22:14:35 -0500

    SQL Injection Parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,19 +8,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+-- GROUP BY cohort
+WHERE cohorts.name = '${process.argv[2]}'
 ORDER BY teacher;
 `)
 .then(res => {
-  console.log(res);
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}:  ${user.teacher}`)
   })
 })
 .catch(err => console.error('query error', err.stack));
